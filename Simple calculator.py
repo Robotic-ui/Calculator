3e3c6c5ba4f61fc7fc6a8d815f3592caab004a0e
Sebastian Rusbacky <84217158+Robotic-ui@users.noreply.github.com>
2024-03-21 19:53:11 -0400

    Update Simple calculator.py

--- Simple calculator.py
+++ Simple calculator.py
@@ -1,3 +1,6 @@
+# 3/21/2024
+# A simple python calculator
+
 def add(x, y):
     return x + y
 
@@ -10,3 +13,42 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+print("*SELECT* operation")
+print("(1) Add")
+print("(2) Subtract")
+print("(3) Multiply")
+print("(4) Divide \n")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice (1, 2, 3 or 4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print('------------------------------')
+        except ValueError:
+            print("Invalid input. Please enter a number! \n")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer no
+        next_calculation = input("Would you like to do another calculation? (Y/n): ")
+        if next_calculation == "n":
+          break
+    else:
+        print("Invalid Input \n")
